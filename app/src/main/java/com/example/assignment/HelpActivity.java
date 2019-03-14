373d0f7da633c0e8b03a2ba2616d21936c32d26b
phanquanganh <phanquanganh61@gmail.com>
2019-03-15 00:54:27 +0700

    Add Font resource file and Help demo screen

--- app/src/main/java/com/example/assignment/HelpActivity.java
+++ app/src/main/java/com/example/assignment/HelpActivity.java
@@ -1,13 +1,57 @@
 package com.example.assignment;
 
+import android.graphics.Color;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.ArrayAdapter;
+import android.widget.ListView;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
 
 public class HelpActivity extends AppCompatActivity {
 
+    ListView listView;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_help);
+        listView = findViewById(R.id.listView);
+
+        String[] questions = new String[]{
+                "How to create new long term plan?",
+                "How to add a short term plan?",
+                "How does the app notify me?"
+        };
+
+        List<String> question_list = new ArrayList<String>(Arrays.asList(questions));
+        final ArrayAdapter<String> arrayAdapter = new ArrayAdapter<String>
+                (this, R.layout.item_text_view, question_list){
+            @Override
+            public View getView(int position, View convertView, ViewGroup parent){
+                // Get the current item from ListView
+                View view = super.getView(position,convertView,parent);
+                ViewGroup.LayoutParams params = view.getLayoutParams();
+
+                // Set the height of the Item View
+                params.height = 140;
+                view.setLayoutParams(params);
+                if(position %2 == 1)
+                {
+                    // Set a background color for ListView regular row/item
+                    view.setBackgroundColor(Color.parseColor("#fd267d"));
+                }
+                else
+                {
+                    // Set the background color for alternate row/item
+                    view.setBackgroundColor(Color.parseColor("#fe5466"));
+                }
+                return view;
+            }
+        };
+        listView.setAdapter(arrayAdapter);
     }
 }
